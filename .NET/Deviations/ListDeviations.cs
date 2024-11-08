5826d4e7d2bdb68e6aa205c865c793d2e18cdb47
Tobias Runesson <162323073+tobiasbof@users.noreply.github.com>
2024-11-08 20:45:44 +0100

    Create ListDeviations.cs

---
+++ .NET/Deviations/ListDeviations.cs
@@ -0,0 +1,37 @@
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+var boardsOnFireApiUrl = "YOUR URL"; // exempel: https://my-company.boardsonfireapp.com/api/v5/
+var apiKey = "YOUR API KEY";
+
+using (var client = new HttpClient())
+{
+    client.BaseAddress = new Uri(boardsOnFireApiUrl);
+    client.DefaultRequestHeaders.Accept.Clear();
+    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    client.DefaultRequestHeaders.Add("x-api-key", apiKey);
+
+    try
+    {
+        var requestBody = new
+        {
+            page = 1,
+            page_size = 10
+        };
+
+        var jsonContent = JsonSerializer.Serialize(requestBody);
+        var httpContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+        var response = await client.PostAsync($"deviations/list", httpContent);
+        response.EnsureSuccessStatusCode();
+
+        var data = await response.Content.ReadAsStringAsync();
+        Console.WriteLine("Deviations:");
+        Console.WriteLine(data);
+    }
+    catch (HttpRequestException e)
+    {
+        Console.WriteLine($"Request error: {e.Message}");
+    }
+}
