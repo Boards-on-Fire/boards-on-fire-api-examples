9b8af03472693390921d1084b133438336121999
Tobias Runesson <162323073+tobiasbof@users.noreply.github.com>
2024-11-08 21:45:31 +0100

    Create ListDataObjects.rb

---
+++ Ruby/DataObjects/ListDataObjects.rb
@@ -0,0 +1,32 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+boards_on_fire_api_url = "YOUR URL"  # example: https://my-company.boardsonfireapp.com/api/v5/
+api_key = "YOUR API KEY"
+data_source_name = "YOUR DATA SOURCE NAME"
+
+uri = URI.parse("#{boards_on_fire_api_url}/datasources/#{data_source_name}/dataobjects/list")
+request = Net::HTTP::Post.new(uri)
+request["Content-Type"] = "application/json"
+request["x-api-key"] = api_key
+
+request_body = {
+  page: 1,
+  page_size: 10
+}
+
+request.body = request_body.to_json
+
+response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+  http.request(request)
+end
+
+if response.is_a?(Net::HTTPSuccess)
+  data = JSON.parse(response.body)
+  puts "DataObjects:"
+  puts data
+else
+  puts "Request error: #{response.message}"
+end
+
