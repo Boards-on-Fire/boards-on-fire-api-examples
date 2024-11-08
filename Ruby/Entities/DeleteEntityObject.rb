664374f8b975bd1e13c50465dacdb545f6613d71
Tobias Runesson <162323073+tobiasbof@users.noreply.github.com>
2024-11-08 21:48:37 +0100

    Create DeleteEntityObject.rb

---
+++ Ruby/Entities/DeleteEntityObject.rb
@@ -0,0 +1,22 @@
+require 'net/http'
+require 'uri'
+
+boards_on_fire_api_url = "YOUR URL"  # example: https://my-company.boardsonfireapp.com/api/v5/
+api_key = "YOUR API KEY"
+entity_name = "YOUR ENTITY NAME"
+entity_object_id = "YOUR ENTITY OBJECT ID"
+
+uri = URI.parse("#{boards_on_fire_api_url}/entities/#{entity_name}/entityObjects/#{entity_object_id}")
+request = Net::HTTP::Delete.new(uri)
+request["Content-Type"] = "application/json"
+request["x-api-key"] = api_key
+
+response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+  http.request(request)
+end
+
+if response.is_a?(Net::HTTPSuccess)
+  puts "EntityObject deleted successfully."
+else
+  puts "Request error: #{response.message}"
+end
