16c98b1601fb28878c56e92c930c6b4c1a9dbe1b
Tobias Runesson <162323073+tobiasbof@users.noreply.github.com>
2024-11-08 21:47:53 +0100

    Create UpdateDeviation.rb

---
+++ Ruby/Deviations/UpdateDeviation.rb
@@ -0,0 +1,30 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+boards_on_fire_api_url = "YOUR URL"  # example: https://my-company.boardsonfireapp.com/api/v5/
+api_key = "YOUR API KEY"
+deviation_id = "YOUR DEVIATION ID"
+
+uri = URI.parse("#{boards_on_fire_api_url}/deviations/#{deviation_id}")
+request = Net::HTTP::Patch.new(uri)
+request["Content-Type"] = "application/json"
+request["x-api-key"] = api_key
+
+request_body = {
+  cause_description: "Something went wrong in production."
+}
+
+request.body = request_body.to_json
+
+response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+  http.request(request)
+end
+
+if response.is_a?(Net::HTTPSuccess)
+  data = JSON.parse(response.body)
+  puts "Deviation:"
+  puts data
+else
+  puts "Request error: #{response.message}"
+end
