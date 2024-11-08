1593e8322d6a9ae6598d7a153856988458ba7287
Tobias Runesson <162323073+tobiasbof@users.noreply.github.com>
2024-11-08 21:34:03 +0100

    Create UpdateEntityObject.go

---
+++ Golang/Entities/UpdateEntityObject.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+    "bytes"
+    "encoding/json"
+    "fmt"
+    "net/http"
+)
+
+func main() {
+    boardsOnFireApiUrl := "YOUR URL" // example: https://my-company.boardsonfireapp.com/api/v5/
+    apiKey := "YOUR API KEY"
+    entityName := "YOUR ENTITY NAME"
+    entityObjectId := "YOUR ENTITY OBJECT ID"
+
+    client := &http.Client{}
+
+    // Entity with string fields production_line and stop_reason
+    // Update stop_reason
+    requestBody := map[string]interface{}{
+        "stop_reason": "no reason",
+    }
+
+    jsonContent, err := json.Marshal(requestBody)
+    if err != nil {
+        fmt.Println("Error marshalling request body:", err)
+        return
+    }
+
+    req, err := http.NewRequest("PATCH", fmt.Sprintf("%s/entities/%s/entityobjects/%s", boardsOnFireApiUrl, entityName, entityObjectId), bytes.NewBuffer(jsonContent))
+    if err != nil {
+        fmt.Println("Error creating request:", err)
+        return
+    }
+
+    req.Header.Set("Content-Type", "application/json")
+    req.Header.Set("x-api-key", apiKey)
+
+    resp, err := client.Do(req)
+    if err != nil {
+        fmt.Println("Request error:", err)
+        return
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode != http.StatusOK {
+        fmt.Println("Request failed with status:", resp.Status)
+        return
+    }
+
+    var data map[string]interface{}
+    if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+        fmt.Println("Error decoding response:", err)
+        return
+    }
+
+    fmt.Println("EntityObject:")
+    fmt.Println(data)
+}
