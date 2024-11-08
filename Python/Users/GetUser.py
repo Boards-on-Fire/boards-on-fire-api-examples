7d33cf956b152916912d2a99a925d46c7d6340b7
Tobias Runesson <162323073+tobiasbof@users.noreply.github.com>
2024-11-08 21:35:56 +0100

    Create GetUser.py

---
+++ Python/Users/GetUser.py
@@ -0,0 +1,19 @@
+import requests
+
+boards_on_fire_api_url = "YOUR URL"  # example: https://my-company.boardsonfireapp.com/api/v5/
+api_key = "YOUR API KEY"
+user_id = "YOUR USER ID"
+
+headers = {
+    "Content-Type": "application/json",
+    "x-api-key": api_key
+}
+
+try:
+    response = requests.get(f"{boards_on_fire_api_url}/users/{user_id}", headers=headers)
+    response.raise_for_status()
+    data = response.json()
+    print("User:")
+    print(data)
+except requests.exceptions.RequestException as e:
+    print(f"Request error: {e}")
