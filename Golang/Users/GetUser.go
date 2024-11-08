e7ddf6ccee6046d42d0a09f299b43d9831cc4be7
Tobias Runesson <162323073+tobiasbof@users.noreply.github.com>
2024-11-08 21:36:01 +0100

    Create GetUser.go

---
+++ Golang/Users/GetUser.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "io/ioutil"
+)
+
+func main() {
+    boardsOnFireApiUrl := "YOUR URL" // example: https://my-company.boardsonfireapp.com/api/v5/
+    apiKey := "YOUR API KEY"
+    userId := "YOUR USER ID"
+
+    client := &http.Client{}
+
+    req, err := http.NewRequest("GET", fmt.Sprintf("%s/users/%s", boardsOnFireApiUrl, userId), nil)
+    if err != nil {
+        fmt.Println("Error creating request:", err)
+        return
+    }
+
+    req.Header.Set("Content-Type", "application/json")
+    req.Header.Set("x-api-key", apiKey)
+
+    resp, err := client.Do(req)
+    if err != nil {
+        fmt.Println("Request error:", err)
+        return
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode != http.StatusOK {
+        fmt.Println("Request failed with status:", resp.Status)
+        return
+    }
+
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        fmt.Println("Error reading response body:", err)
+        return
+    }
+
+    fmt.Println("User:")
+    fmt.Println(string(body))
+}
