752eeb21e6a09af29ff726469ba5611479c4e7ea
Tobias Runesson <162323073+tobiasbof@users.noreply.github.com>
2024-11-08 19:53:21 +0100

    Update GetUsers.cs

--- .NET/Users/GetUsers.cs
+++ .NET/Users/GetUsers.cs
@@ -12,7 +12,7 @@ using (var client = new HttpClient())
 
     try
     {
-        HttpResponseMessage response = await client.GetAsync("users");
+        var response = await client.GetAsync("users");
         response.EnsureSuccessStatusCode();
 
         var data = await response.Content.ReadAsStringAsync();
